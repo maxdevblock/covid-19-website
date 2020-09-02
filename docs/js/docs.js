dbfe978840b64b7356d911ddaa330c8be2ac9353
Max Pierini <gitlab@maxpierini.it>
2020-09-02 19:12:49 +0200

    do not execute

--- docs/js/docs.js
+++
@@ -1,9 +0,0 @@
-var ItaliaArray = ['Crescita', 'Cumulativi', 'Immuni', 'Percent Positive', 'Previsioni', 'Province', 'Variazioni', 'stime Rt']
-var ItaliaDict = {'Stime-di-Crescita-1': 'Crescita', 'Dati-cumulativi-1': 'Cumulativi', 'App-Immuni-1': 'Immuni', 'Tasso-di-Positivit-1': 'Percent Positive', 'Modello-Predittivo-a-7gg-1': 'Previsioni', 'Casi-nelle-Province-1': 'Province', 'Variazioni-1': 'Variazioni', 'Numero-di-Riproduzione-1': 'stime Rt'}
-var ItaliaArrayMap = {'Crescita': 'Stime di Crescita', 'Cumulativi': 'Dati cumulativi', 'Immuni': 'App Immuni', 'Percent Positive': 'Tasso di Positività', 'Previsioni': 'Modello Predittivo a 7gg', 'Province': 'Casi nelle Province', 'Variazioni': 'Variazioni', 'stime Rt': 'Numero di Riproduzione'}
-var NazioniArray = ['Panoramica', 'Single', 'Sommario']
-var NazioniDict = {'Overview-1': 'Panoramica', 'Single-1': 'Single', 'All-Countries-SIRD-1': 'Sommario'}
-var NazioniArrayMap = {'Panoramica': 'Overview', 'Single': 'Single', 'Sommario': 'All Countries SIRD'}
-var ContinentiArray = ['Panoramica', 'Single']
-var ContinentiDict = {'Overview-1': 'Panoramica', 'Single-1': 'Single'}
-var ContinentiArrayMap = {'Panoramica': 'Overview', 'Single': 'Single'}
